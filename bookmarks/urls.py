2af59d767fb05965d77cf6acc492d5432e29a790
awelekan <awe_lekan@yahoo.com>
2024-12-25 15:51:27 +0100

    First commit

---
+++ bookmarks/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+
+
+
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('account/', include('account.urls')),
+path(
+ 'social-auth/',
+ include('social_django.urls', namespace='social')
+ ),
+
+]
